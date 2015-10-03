f5652f65dac60de42b9fc93ed500ecf500cdc70d
Dustin Biser <dustinbiser@gmail.com>
2015-10-02 23:27:32 -0400

    Spaces to tabs within CheckDxError.hpp

--- Shared/Utils/CheckDxError.hpp
+++ Shared/Utils/CheckDxError.hpp
@@ -4,7 +4,7 @@
 
 
 /**
- *  Can be used with any DirectX function that returns an HRESULT.
+ *	Can be used with any DirectX function that returns an HRESULT.
  *	Checks the HRESULT x. If FAILED(x), a MessageBox is displayed with DirectX
  *	error message, and an option to drop into debug mode at the line of failure.
  */
